ef6a39f186be821e1e6e71eab5b9248dae483bdb
isrzian <shadow-sergej@ya.ru>
2022-03-10 11:49:37 +0700

    server ws begin

--- server/index.js
+++ server/index.js
@@ -1,10 +1,34 @@
 const express = require('express')
-const cors = require('cors')
-
 const app = express()
+const wsServer = require('express-ws')(app)
+const aWss = wsServer.getWss()
 
-app.use(cors())
+app.ws('/', (ws, req) => {
+	console.log('Connection initialized!')
+	ws.send('You successfully connected!')
+	ws.on('message', (msg) => {
+		msg = JSON.parse(msg)
+		switch (msg.method) {
+			case 'connection':
+				connectionHandler(ws, msg)
+				break
+		}
+	})
+})
 
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => console.log(`Server has been started on ${PORT} port...`))
+
+const connectionHandler = (ws, msg) => {
+	ws.id = msg.id
+	broadcastConnection(ws, msg)
+}
+
+const broadcastConnection = (ws, msg) => {
+	aWss.clients.forEach(client => {
+		if (client.id === msg.id) {
+			client.send(`User ${msg.username} connected`)
+		}
+	})
+}
